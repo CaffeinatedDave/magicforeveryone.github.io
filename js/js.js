d19e822eff571a1eff6fe7641c5e5c2e3a81a66b
Dave Chaston <dave@caffeinateddave.com>
2020-01-20 20:39:44 +0000

    Actually add new js file

---
+++ js/js.js
@@ -0,0 +1,56 @@
+$().ready(function() {
+  if (getCookie('gdpr') != 1) {
+    $('#gdprFooter').show();
+  }
+})
+
+function getCookie(key) {
+  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+  return keyValue ? keyValue[2] : null;
+}
+function setCookie(key, value, expiry) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+function saveGDPRandClose() {
+  $('#gdprFooter').remove();
+  setCookie('gdpr', 1, 400);
+  return false;
+}
+
+// Open the Modal
+function openModal(str) {
+  document.getElementById(str).style.display = "block";
+}
+
+// Close the Modal
+function closeModal(str) {
+  document.getElementById(str).style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
